9c0bcfa7c1df2937d31b5014d34c5b2a4e161f27
Rock Boynton <rock.boynton@yahoo.com>
2022-03-28 22:18:29 -0700

    Use powershell install script over batch

---
+++ install.ps1
@@ -0,0 +1,79 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+function Check-Command($cmdname) {
+    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
+# -----------------------------------------------------------------------------
+$computerName = Read-Host 'Enter New Computer Name'
+Write-Host "Renaming this computer to: " $computerName  -ForegroundColor Yellow
+Rename-Computer -NewName $computerName
+
+# -----------------------------------------------------------------------------
+Write-Host ""
+Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Powercfg /Change monitor-timeout-ac 20
+Powercfg /Change standby-timeout-ac 0
+
+# -----------------------------------------------------------------------------
+Write-Host ""
+Write-Host "Enable Windows 10 Developer Mode..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
+
+# -----------------------------------------------------------------------------
+if (Check-Command -cmdname 'choco') {
+    Write-Host "Choco is already installed, skip installation."
+} else {
+    Write-Host ""
+    Write-Host "Installing Chocolate for Windows..." -ForegroundColor Green
+    Write-Host "------------------------------------" -ForegroundColor Green
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Write-Host ""
+Write-Host "Installing Applications..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+
+$Apps = @(
+    "7zip.install",
+    "git",
+    "googlechrome",
+    "vlc",
+    "vscode",
+    "microsoft-teams.install"
+)
+
+foreach ($app in $Apps) {
+    choco install $app -y
+}
+
+# TODOs in no particular order --------------------------------------------------------
+
+# TODO setup git
+
+# TODO create SSH key
+
+# TODO install fira code
+
+# TODO setup VS code (extensions, setting.json)
+
+# TODO install windows terminal (add settings.json, etc)
+
+# TODO install WSL2
+
+# TODO setup WSL2
+
+# -----------------------------------------------------------------------------
+Write-Host ""
+Write-Host "Checking Windows updates..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Install-Module -Name PSWindowsUpdate -Force
+Write-Host "Installing updates... (Computer will reboot in minutes...)" -ForegroundColor Green
+Get-WindowsUpdate -AcceptAll -Install -ForceInstall -AutoReboot
+
+# -----------------------------------------------------------------------------
+Write-Host "------------------------------------" -ForegroundColor Green
+Read-Host -Prompt "Setup is done, restart is needed, press [ENTER] to restart computer."
+Restart-Computer
