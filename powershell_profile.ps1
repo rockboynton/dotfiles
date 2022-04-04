1b2424da0907e26f54b2e3adeb9749556864465a
Rock Boynton <rock.boynton@yahoo.com>
2022-04-03 23:04:02 -0700

    Add choco profile to powershell profile

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,1 +1,7 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
 Invoke-Expression (&starship init powershell)
