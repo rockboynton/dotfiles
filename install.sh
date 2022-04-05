724f2c424afefd238520d3c74a95f90872e647c0
Rock Boynton <rock.boynton@yahoo.com>
2022-04-04 23:42:28 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-# TODO create SSH key follow https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
-
 email="rock.boynton@yahoo.com"
+# TODO read username and email from input instead
 
 sudo apt update
 sudo apt upgrade -y
+
+# install all the things
+# sudo apt install
 
 # generate ssh key
 ssh-keygen -t ed25519 -C "$email" -f "$HOME"/.ssh/id_ed25519 -q -P
